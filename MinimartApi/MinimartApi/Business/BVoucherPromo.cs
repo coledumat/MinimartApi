fb09ac1cfb35a02bc40a1e3923819c6e392aebb0
coledumat <coledumat@hotmail.com>
2022-05-10 17:09:54 -0300

    VoucherProductController add POST and GET

---
+++ MinimartApi/MinimartApi/Business/BVoucherPromo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+using MinimartApi.Models;
+
+
+namespace MinimartApi.Business
+{
+    public class BVoucherPromo : BusinessClass
+    {
+        /*
+          Business Class VoucherPromo.
+        */
+
+        public int CreateProductVoucherPromo( ProductVoucherPromoModel newProductVoucherPromo) 
+        {
+            //insert into VoucherPromo and ProductVoucherPromo Tables
+            string sql = "INSERT INTO VoucherPromo (id_Minimart, VIniNumber, VEndNumber, Name, Description, StartDate, EndDate, WeekDays, StartingWithXUnits, UnitOnDiscount, PercentageDiscount)" +
+                         " Values (@id_Minimart, @VIniNumber, @VEndNumber, @Name, @Description, @StartDate, @EndDate, @WeekDays, @StartingWithXUnits, @UnitOnDiscount, @PercentageDiscount);";
+
+            int affectedRows = 0;
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                affectedRows =  connection.Execute(sql, new { 
+                                                            id_Minimart = newProductVoucherPromo.MinimartId ,
+                                                            VIniNumber = newProductVoucherPromo.VIniNumber,
+                                                            VEndNumber = newProductVoucherPromo.VEndNumber,
+                                                            Name = newProductVoucherPromo.Name,
+                                                            Description = newProductVoucherPromo.Description,
+                                                            StartDate= newProductVoucherPromo.StartDate,
+                                                            EndDate = newProductVoucherPromo.EndDate ,
+                                                            WeekDays = string.Join(",",newProductVoucherPromo.WeekDays),
+                                                            StartingWithXUnits= newProductVoucherPromo.StartingWithXUnits,
+                                                            UnitOnDiscount = newProductVoucherPromo.UnitOnDiscount ,
+                                                            PercentageDiscount = newProductVoucherPromo.PercentageDiscount 
+                                                            });
+
+                //Console.WriteLine(affectedRows);
+
+                //read identity id_Voucher
+                sql = "SELECT MAX(Id) FROM VoucherPromo;";
+                var new_id_Voucher = connection.QuerySingle<int>(sql, new {});  //connection.Query<Customer>("Select Max(id) FROM VoucherPromo") //.ToList();
+
+                if (affectedRows == 1 && new_id_Voucher > 0)
+                {
+                    foreach (var product in newProductVoucherPromo.includeProducts)
+                    {
+
+                        //insert include products
+                        sql = "INSERT INTO ProductVoucherPromo(id_Voucher, Id_Product)" +
+                              " Values ( @id_Voucher, @Id_Product);";
+                        affectedRows = connection.Execute(sql, new
+                        {
+                            id_Voucher = new_id_Voucher,
+                            id_Product = product.ProductId
+                        });
+                    }
+                }
+            }
+
+           return affectedRows;
+
+        }
+
+
+        
+        public IEnumerable<ProductVoucherPromo> listProductVoucherPromos(int minimartId, string minimartName, 
+                                                                         int voucherId,
+                                                                         int categoryId, string categoryName, int productId, string productName)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString ))
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("minimart_id", minimartId);
+                parameters.Add("minimart_name", minimartName);
+                parameters.Add("voucher_id", voucherId);
+                parameters.Add("category_id", categoryId);
+                parameters.Add("category_name", categoryName);
+                parameters.Add("product_id", productId);
+                parameters.Add("product_name", productName);
+
+                var virtualCartProductVouchers = connection.Query<ProductVoucherPromo>("SP_ProductVoucherPromo", param: parameters, commandType: CommandType.StoredProcedure);
+                return virtualCartProductVouchers;
+            }
+
+        }
+      
+
+
+
+
+    }
+
+}
---
+++ db/scripts/dbo.SP_ProductVoucherPromo.sql
@@ -0,0 +1,54 @@
+﻿/*
+return all VirtualCart_Vouvher  (CategoryVoucherPromo and ProductVucherPromo)
+filters: product id or name / category id or name
+*/
+
+CREATE   PROCEDURE [dbo].[SP_ProductVoucherPromo]
+    @minimart_id int = 0,
+	@minimart_name nchar(200) = '',
+	@voucher_id int = 0,
+	@category_id int = 0,             
+	@category_name nchar(200) = '', 
+	@product_id int = 0,              
+	@product_name nchar(200) = ''    
+
+AS
+
+begin 
+	SELECT m.Id as minimartId, 
+	       LTrim(RTrim(m.Name)) as minimartName,
+		   --
+		   vp.Id as VoucherId,
+		   vp.VIniNumber,
+		   vp.VEndNumber, 
+		   LTrim(RTrim(vp.Name)) as VoucherName,
+		   LTrim(RTrim(vp.Description)) as VoucherDescription,
+		   vp.StartDate,
+		   vp.EndDate,
+		   LTrim(RTrim(vp.WeekDays)) as WeekDays,
+		   vp.StartingWithXUnits,
+		   vp.UnitOnDiscount,
+		   vp.PercentageDiscount,
+		   vp.TipoVoucher,
+		   --
+		   c.Id as categoryId,
+		   LTrim(RTrim(c.Name)) as categoryName,
+		   p.Id as productId, 
+		   LTrim(RTrim(p.Name)) as productName,
+		   p.Price
+	  FROM VoucherPromo vp
+	  join Minimart m on (m.Id = vp.Id_Minimart) 
+	  --
+	  join ProductVoucherPromo pvp on (pvp.Id_Voucher = vp.Id)
+	  join Product p on (p.Id = pvp.Id_Product)
+	  join Product_Category pc on (p.Id = pc.Id_Product )
+      join Category c on ( c.Id = pc.id_Category)
+     WHERE ((m.Id = @minimart_id or @minimart_id = 0) and (Upper(LTrim(RTrim(m.Name))) Like  '%' + Upper(LTrim(RTrim(@minimart_name))) + '%' or  @minimart_name = ''))
+       --
+       and (vp.Id = @voucher_id or @voucher_id = 0)
+	   --
+	   and ((p.Id = @product_id or @product_id = 0) and (Upper(LTrim(RTrim(p.Name))) Like '%' + Upper(LTrim(RTrim(@product_name))) + '%' or  @product_name = ''))
+	   and ((c.Id =@category_id or @category_id = 0) and (Upper(LTrim(RTrim(c.Name))) Like '%' + Upper(LTrim(RTrim(@category_name))) + '%' or  @category_name = ''))
+    ORDER BY m.Id, vp.Id, c.Name, p.Name 
+
+end
